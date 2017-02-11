4d8379a37ee6aef2cc3457a1073ef0ccede67dc5
SebastianPetak <sebastian.petak@gmail.com>
2017-02-11 13:44:18 -0600

    TextContainer.js: Text string is difficult to read. Rewrite the text string variable to utilize template literals. Install babel-preset-es2015 to transform the es2015 template literal feature into es5 for the UglifyJS module. Modify the .babelrc file to include es2015.

--- app/containers/TextContainer.js
+++ app/containers/TextContainer.js
@@ -9,12 +9,37 @@ var marked = require('marked');
 
 var TextContainer = React.createClass({
 	getInitialState: function() {
-		var text = '# H1\n## H2\n## H3\n\nLine\nBreak\n\nvs\n\nLine (two spaces)  '+
-			'\nBreak\n\n *Emphasis* , aka _italics\n**Strong** ' +
-			'emphasis, aka __bold__**\n_Combined_**\n\n1. list\n' +
-			'  * unordered sub-list\n\n\n2. Moar list\n'+
-			'  1. Ordered sub-list\n  2. One moar\n  3. _...One moar_\n\n' +
-			'`inline code`\n\n```javascript\nMuch\nCode\n```\n> Blockquotes';
+		//Initial text inside the textarea.
+		var text = `\
+# H1
+## H2
+## H3
+
+Line
+Break
+
+vs
+
+Line (two spaces)  \nBreak
+
+*Emphasis* , aka _italics **Strong** emphasis, aka __bold__** _Combined_**
+
+1. list
+ * unordered sub-list
+
+
+2. Moar list
+ 1. Ordered sub-list
+ 2. One moar
+ 3. _...One moar_
+
+\`inline code\`
+
+\`\`\`javascript
+Much
+Code
+\`\`\`
+> Blockquotes;`;
 		return {
 			text: text,
 		};
